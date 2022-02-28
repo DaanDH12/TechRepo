f8b6496b44de7542b63aa2d15004a2b52a93c3c6
DaanDH12 <daanhoning@kpnmail.nl>
2022-02-28 15:42:17 +0100

    Added Readme Files

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/about', (req,res) => {
+  res.send('About')
+})
+
+app.get('/login', (req,res) => {
+  res.send('Login')
+})
+
+app.get('*', (req,res) => {
+  res.send('404 Not Found')
 })
 
 app.listen(port, () => {
