5d515cc083ade7f8e504b8ddf49e52d19629c2fd
DaanDH12 <daanhoning@kpnmail.nl>
2022-03-02 09:35:46 +0100

    Updated Repo

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const { engine } = require('express-handlebars');
+const app = express();
 
-app.use(express.static('public'))
+const path = require('path');
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+app.engine('.hbs', engine({
+  extname: '.hbs',
+  defaultLayout: 'main'
+}));
+app.set('view engine', '.hbs');
+app.set("views", "./views");
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.render('index');
+});
 
-app.get('/about', (req,res) => {
-  res.send('About')
-})
-
-app.get('/login', (req,res) => {
-  res.send('Login')
-})
-
-app.get('*', (req,res) => {
-  res.send('404 Not Found')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(3000);
