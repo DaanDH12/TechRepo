a222f10318d7ac6d4d4d8c074e36fef0241ea284
DaanDH12 <daanhoning@kpnmail.nl>
2022-03-04 16:26:04 +0100

    Added body-parser and multi

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const { engine } = require('express-handlebars');
 const app = express();
-
+const bodyParser = require('body-parser')
 
 const path = require('path');
 app.use('/static', express.static(path.join(__dirname, 'public')));
@@ -20,5 +20,17 @@ app.get('/', (req, res) => {
 app.get('/registreren', (req, res) => {
   res.render('registreren');
 });
+
+// bodyparser codes 
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
+
+app.use(function (req, res) {
+  res.setHeader('Content-Type', 'text/plain')
+  res.write('you succesfully logged in:\n')
+  res.end(JSON.stringify(req.body, null, 2))
+})
 
 app.listen(3000);
